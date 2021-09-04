6359d5238618f0530687e59179015654ec03328f
Barry-Thomas <barryharoldthomas@gmail.com>
2021-09-05 03:13:12 +0400

    Setup basic DbContext

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Publisher { get; set; }
+
+        public string Cost { get; set; }
+
+        [Required]
+        public DateTime Created { get; set; }
+
+        [Required]
+        public DateTime Updated { get; set; }
+    }
+}
